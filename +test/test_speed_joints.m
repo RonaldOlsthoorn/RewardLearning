8ee3ad7e25357410d4c332e6c0bf51469bb8010f
Ronald Olsthoorn <ronaldolsthoorn1@gmail.com>
2016-09-14 11:22:10 +0200

    Speed joints test now working. External controller not working completely perfect. Speed tools test added. Will not be using it.

--- +test/test_speed_joints.m
+++ +test/test_speed_joints.m
@@ -9,9 +9,9 @@ arm.fopen(ip);
 UR5.reset_arm(arm);
 tolerance = 0.001;
 
-%pause(1);
+pause(1);
 
-i = 6;
+for i = 1:6;
 
 a_tol = 2*ones(6, 1);
 v_tol = 1*ones(6, 1);
@@ -20,8 +20,8 @@ Ts = 0.1;
 arm.update();
 posFrom = arm.getJointsPositions();
 posTo = posFrom;
-posTo(i) = posTo(i) + pi/16;
-duration = 4;
+posTo(i) = posTo(i) - pi/8;
+duration = 8;
 
 dmp_par.n_dmp_bf = 10;
 dmp_par.Ts = Ts;
@@ -45,7 +45,7 @@ t_command = 0;
 y_buf = posFrom;
 t_buf = 0;
 v_buf = zeros(6, 1);
-a_buf = zeros(6, 1);
+a_buf = zeros(1, 1);
 
 pos = posFrom;
 vel = zeros(6, 1);
@@ -56,13 +56,13 @@ for j = 2:length(y(:,1))
     
     a_d = abs((vel_d(:, j)-vel)/(Ts));
 
-%     if a_d(i) > a_tol(i) 
-%         a_d(i) = a_tol;
-%         disp('Warning: acceleration limit reached!!');
-%     elseif a_d(i) < -a_tol(i) 
-%         a_d(i) = -a_tol;
-%         disp('Warning: acceleration limit reached!!');
-%     end
+    if a_d(i) > a_tol(i) 
+        a_d(i) = a_tol;
+        disp('Warning: acceleration limit reached!!');
+    elseif a_d(i) < -a_tol(i) 
+        a_d(i) = -a_tol;
+        disp('Warning: acceleration limit reached!!');
+    end
     
     v_d = (pos_d(:, j) - pos) / Ts;
     
@@ -73,19 +73,14 @@ for j = 2:length(y(:,1))
         v_d(i) = -v_tol(i);
         disp('Warning: velocity limit reached!!');
     end
-    
-    a_d = flipud(a_d);
-    a_d(2:6, 1) = 0;
-    v_d(1:5, 1) = 0;
-    
-    
-    arm.setJointsSpeed(v_d, a_d, Ts);
+        
+    arm.setJointsSpeed(v_d, a_d(i, 1), Ts);
     
     t = tic;
     t_command(end + 1) = toc(t0);
     
     v_buf(:, end + 1) = v_d;
-    a_buf(:, end + 1) = a_d;
+    a_buf(1, end + 1) = a_d(i, 1);
     
     while toc(t) < Ts
        
@@ -93,20 +88,28 @@ for j = 2:length(y(:,1))
     
     arm.update();
     
-    pos = arm.getJointsPositions();
+    pos = arm.getJointsPositions()
     vel = arm.getJointsSpeeds();
     
     y_buf(:, end + 1) = pos;
     t_buf(end + 1) = toc(t0);
 end
 
+
+
+disp('out of the movement loop');
+
 bufPos{i} = y_buf;
 bufT{i} = t_buf;
-% bufV{i} = v_buf;
-% bufA{i} = a_buf;
+bufV{i} = v_buf;
+bufA{i} = a_buf;
 
-clear acc t_buf y_buf vel pos a_d v_d t t_command;
-clear t0 y yd ydd i j;
+pause(1);
+
+end
 
 UR5.reset_arm(arm);
 arm.fclose();
+
+clear acc t_buf y_buf vel pos a_d v_d t t_command;
+clear t0 y yd ydd i j;
--- +test/test_speed_tool.m
+++ +test/test_speed_tool.m
@@ -13,19 +13,19 @@ pause(1);
 
 i = 1;
 
-a_tol = 2;
-v_tol = 1;
+a = 1;
+v_tol = 1*ones(6, 1);
 Ts = 0.1;
 
 arm.update();
 posFrom = arm.getToolPositions();
 posTo = posFrom;
-posTo(i) = posTo(i) + 0.1;
-duration = 2;
+posTo(i) = posTo(i) - 0.1;
+duration = 8;
 
 dmp_par.n_dmp_bf = 10;
 dmp_par.Ts = Ts;
-dmp_par.duration = 4;
+dmp_par.duration = duration;
 dmp_par.start = posFrom(i);
 dmp_par.goal = posTo(i);
 
@@ -42,58 +42,62 @@ acc_d(i, :) = ydd;
 
 t0 = tic;
 t_command = 0;
-y_buf = posFrom(i);
+y_buf = posFrom;
 t_buf = 0;
+v_buf = zeros(6, 1);
+a_buf = zeros(1, 1);
 
 pos = posFrom;
 vel = zeros(6, 1);
-acc = zeros(6, 1);
 
-for j = 2:length(y(:,1))
+disp('into the movement loop');
+
+for j = 2:length(y(:, 1))
     
-    a_d = (vel_d(:, j)-vel)/(2*Ts);
-    
-    if a_d > a_tol
-        a_d = a_tol;
-        disp('Warning: acceleration limit reached!!');
-    elseif a_d < -a_tol
-        a_d = -a_tol;
-        disp('Warning: acceleration limit reached!!');
-    end
+    a_d = 1;
     
     v_d = (pos_d(:, j) - pos) / Ts;
     
-    if v_d > v_tol
-        v_d = v_tol;
+    if v_d(i) > v_tol(i)
+        v_d(i) = v_tol(i);
         disp('Warning: velocity limit reached!!');
-    elseif v_d < -v_tol
-        v_d = -v_tol;
+    elseif v_d(i) < -v_tol(i) 
+        v_d(i) = -v_tol(i);
         disp('Warning: velocity limit reached!!');
     end
-    
-    arm.setJointsSpeed((pos_d(:, j) - pos) / Ts, a_d, Ts);
+        
+    arm.setToolSpeed(v_d, a_d, Ts);
     
     t = tic;
     t_command(end + 1) = toc(t0);
     
+    v_buf(:, end + 1) = v_d;
+    a_buf(1, end + 1) = a_d;
+    
     while toc(t) < Ts
-        pos = arm.getToolPositions();
-        t_buf(end + 1) = toc(t0);
-        y_buf(end + 1) = pos(i);
-        arm.update();
+       
     end
     
     arm.update();
+    
     pos = arm.getToolPositions();
     vel = arm.getToolSpeeds();
+    
+    y_buf(:, end + 1) = pos;
+    t_buf(end + 1) = toc(t0);
 end
+
+disp('out of the movement loop');
 
 bufPos{i} = y_buf;
 bufT{i} = t_buf;
+bufV{i} = v_buf;
+bufA{i} = a_buf;
 
-pause(1);
-UR5.reset_arm(arm);
 pause(1);
 
 UR5.reset_arm(arm);
 arm.fclose();
+
+clear acc t_buf y_buf vel pos a_d v_d t t_command;
+clear t0 y yd ydd i j;
