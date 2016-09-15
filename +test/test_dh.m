e59fdb21b1498fe0d82fec3ab06225c92b4ee217
Ronald Olsthoorn <rmolsthoorn@tudelft.net>
2016-09-15 17:04:30 +0200

    forward part works at least for two different points

---
+++ +test/test_dh.m
@@ -0,0 +1,16 @@
+function [model_UR5, T, u] = test_dh(input)
+
+mdl_UR5;
+
+corrected_input = input;
+corrected_input(2) = corrected_input(2);
+corrected_input(4) = corrected_input(2);
+
+T = model_UR5.fkine(corrected_input);
+u = T*[0;0;0;1];
+
+figID = 1;
+figure(double(figID));
+set(double(figID), 'units','normalized','outerposition',[0 0 1 1]);
+clf;
+model_UR5.plot(corrected_input);
--- rvctools/robot/mdl_UR5.m
+++ rvctools/robot/mdl_UR5.m
@@ -1,17 +1,18 @@
-clear L
+clear L;
+
 L(1) = Link('revolute', 'd', 0.089159, 'a', 0, 'alpha', pi/2,...
     'offset', 0);
- L(1).flip = true;
+L(1).flip = true;
 
 L(2) = Link('revolute', 'd', 0, 'a', -0.42500, 'alpha', 0,...
-    'offset', -pi/2);
- L(2).flip = true;
+    'offset', -pi);
+L(2).flip = true;
 
 L(3) = Link('revolute', 'd', 0, 'a', -0.39225, 'alpha', 0,...
     'offset', 0);
 
 L(4) = Link('revolute', 'd', 0.10915, 'a', 0, 'alpha', pi/2,...
-    'offset', -pi/2);
+    'offset', pi/2);
 
 L(5) = Link('revolute', 'd', 0.09465, 'a', 0, 'alpha', -pi/2,...
     'offset', 0);
