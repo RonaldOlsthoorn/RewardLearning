6a1fa9fed9ec180283c48b81c7cca33473736150
Ronald Olsthoorn <ronaldolsthoorn1@gmail.com>
2016-09-20 15:08:59 +0200

    init of reward model fixed

--- +expert/query_expert.m
+++ +expert/query_expert.m
@@ -3,11 +3,11 @@ function [ R_expert ] = query_expert( sum_out, segment, noise )
 % for reward function) of a trajectory
 % noise: standard deviation of the expert rating uncertainty
 
-w_true = [0.1 1 2 4
-          0.1 1 2 4];
+% w_true = [0.1 1 2 4
+%           0.1 1 2 4];
       
-%w_true = [1 1 1 1
-%          1 1 1 1];
+w_true = [1 1 1 1
+          1 1 1 1];
       
 R_expert = w_true(:,segment)'*sum_out' + noise*randn(1,1);
 
