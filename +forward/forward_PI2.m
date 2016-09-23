c8b79d39f4e72f13fc08d56eeab9e08670a10627
Ronald Olsthoorn <rmolsthoorn@tudelft.net>
2016-09-23 16:04:16 +0200

    pre push commit

---
+++ +forward/forward_PI2.m
@@ -0,0 +1,21 @@
+classdef forward_PI2
+    %UNTITLED6 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        
+        policy;
+        std;
+        annealer;
+        noise_mult;
+        
+    end
+    
+    methods
+        
+        update(S);
+        
+        update_noise()
+    end
+    
+end
--- +protocols/forward_PI2.m
+++ +protocols/forward_PI2.m
@@ -9,4 +9,3 @@ forward_rl_par.reps = 10;
 forward_rl_par.n_reuse = 5;
 forward_rl_par.n_dmp_bf = 40;
 end
-
